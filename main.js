6578f9482deb726f106554219bd0802d00cae50c
eAreebk <156355720+eAreebk@users.noreply.github.com>
2024-03-02 20:24:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -163,7 +163,7 @@ function update() {
     console.log(guess);
 
     if (!guessList.includes(guess)) {
-        document.getElementById("answer").innerText = "Not in word list";
+        document.getElementById("answer").innerText = "Not found";
         return;
     }
     
